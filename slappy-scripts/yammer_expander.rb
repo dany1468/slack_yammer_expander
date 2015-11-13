eb70de81c0ac8c953fce7defb9be9e00220146a1
dany1468 <dany1468@gmail.com>
2015-11-13 11:07:10 +0900

    Rename yammer network name env key

--- slappy-scripts/yammer_expander.rb
+++ slappy-scripts/yammer_expander.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'yammer'
 require './config/yammer'
 
-hear "https://www.yammer.com/#{ENV['YAMMER_GROUP_NAME']}/#/Threads/show\\?threadId=(\\d*)" do |event|
+hear "https://www.yammer.com/#{ENV['YAMMER_NETWORK_NAME']}/#/Threads/show\\?threadId=(\\d*)" do |event|
   thread_id = event.matches[1]
 
   thread = Yammer::Resources::Thread.get(thread_id.to_i)
