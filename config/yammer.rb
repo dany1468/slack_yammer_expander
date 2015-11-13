8f9c7ad49ddb2ca9fb30075993ec5e5407e3949a
dany1468 <dany1468@gmail.com>
2015-11-13 09:57:30 +0900

    first commit

---
+++ config/yammer.rb
@@ -0,0 +1,5 @@
+Yammer.configure do |c|
+  c.client_id = ENV['YAMMER_CLIENT_ID']
+  c.client_secret = ENV['YAMMER_CLIENT_SECRET']
+  c.access_token = ENV['YAMMER_ACCESS_TOKEN']
+end
---
+++ slappy-scripts/yammer_expander.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require 'yammer'
+require './config/yammer'
+
+hear "https://www.yammer.com/#{ENV['YAMMER_GROUP_NAME']}/#/Threads/show\\?threadId=(\\d*)" do |event|
+  thread_id = event.matches[1]
+
+  thread = Yammer::Resources::Thread.get(thread_id.to_i)
+  body = thread.messages.to_a.last.last.body
+
+  say body[:plain], channel: event.channel
+end
